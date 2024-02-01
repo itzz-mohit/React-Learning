dfb1ad114f9d6477a710fdfc0eaa1d8a6de7236c
Mohit <mohitbite@gmail.com>
2024-02-01 17:27:35 +0530

    done with tailwind

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 p-4 rounded-xl text-black">Tailwind Test</h1>
+    </>
+  );
+}
+
+export default App;
