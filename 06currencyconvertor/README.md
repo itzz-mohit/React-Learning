6d87e2d303a5a5ab17fd70f81ba8580d8884ca26
Mohit <mohitbite@gmail.com>
2024-02-06 15:19:20 +0530

    useId Hook

--- 06currencyconvertor/README.md
+++ 06currencyconvertor/README.md
@@ -1,8 +1,20 @@
-# React + Vite
+# Key Learning: Currency Converter App
+A simple React-based currency converter application that allows users to convert amounts from one currency to another. This project includes custom input boxes, API fetching, and a clean user interface.
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Custom Input Boxes
+Implemented bespoke input boxes with customizable parameters, ensuring adaptability across various pages while maintaining a consistent and intuitive user interface.
 
-Currently, two official plugins are available:
+## useId Hook
+Mitigated potential DOM manipulation warnings by employing the `useId hook` to generate `unique IDs` for input boxes, promoting a smooth and error-free user experience.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Custom Hooks for API Integration
+Developed a custom hook (useCurrencyInfo) to efficiently fetch currency information from an API, encapsulating the logic and providing the data in a structured array format.
+
+## Currency Conversion Functionality
+Enables users to input an amount, select source and target currencies, and seamlessly perform accurate currency conversions, enhancing the overall utility of the application.
+
+## Intuitive Swap Feature
+Incorporated an intuitive "swap" button that allows users to effortlessly interchange the source and target currencies, optimizing the efficiency of the conversion process.
+
+## Stylish User Interface
+Designed an aesthetically pleasing user interface with a dynamic background image, enhancing the overall visual appeal of the application.
--- 08creditCard/src/App.jsx
+++ 08creditCard/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
@@ -7,6 +7,91 @@ function App() {
   const [month, setMonth] = useState("00");
   const [year, setYear] = useState("00");
   const [cvc, setCvc] = useState(123);
+  const [nameError, setNameError] = useState("");
+  const [cardNumberError, setCardNumberError] = useState("");
+  const [monthError, setMonthError] = useState("");
+  const [yearError, setYearError] = useState("");
+  const [cvcError, setCvcError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const enteredName = e.target.elements.name.value.trim();
+    const enteredCardNumber = e.target.elements.cardNumber.value.trim();
+    const enteredMonth = e.target.elements.month.value.trim();
+    const enteredYear = e.target.elements.year.value.trim();
+    const enteredCvc = e.target.elements.cvc.value.trim();
+
+    if (!isValidName(enteredName)) {
+      setNameError("Invalid CardHolder Name");
+      return;
+    }
+
+    // if (!isValidNumber(enteredCardNumber) || enteredCardNumber.length !== 16) {
+    //   setCardNumberError("Invalid Card Number");
+    //   return;
+    // }
+
+    if (!isValidNumber(enteredCardNumber) || enteredCardNumber.length !== 10) {
+      setCardNumberError("Invalid Card Number");
+      return;
+    }
+    
+    if (!isValidNumber(enteredMonth)) {
+      setMonthError("Invalid Month");
+      return;
+    }
+
+    if (!isValidNumber(enteredYear)) {
+      setYearError("Invalid Year");
+      return;
+    }
+
+    if (enteredCvc.length !== 3) {
+      setCvcError("Invalid CVC");
+      return;
+    }
+
+    setNameError("");
+    setName(enteredName);
+    setCardNumberError("");
+    setCardNumber(enteredCardNumber);
+    setMonthError("");
+    setMonth(enteredMonth);
+    setYearError("");
+    setYear(enteredYear);
+    setCvcError("");
+    setCvc(enteredCvc);
+
+    formReset.current.reset();
+
+    // const data = {
+    //   cardHolderName: name,
+    //   cardNumber: cardNumber,
+    //   month: month,
+    //   year: year,
+    //   cvc: cvc,
+    // };
+
+    const data = {
+      cardHolderName: enteredName,
+      cardNumber: enteredCardNumber,
+      month: enteredMonth,
+      year: enteredYear,
+      cvc: enteredCvc,
+    };
+    console.log(data);
+  };
+
+  const formReset = useRef(null);
+
+  const isValidName = (str) => {
+    return /^[A-Za-z]+$/.test(str);
+  };
+
+  const isValidNumber = (str) => {
+    return /^\d+$/.test(str);
+  };
 
   return (
     <>
@@ -27,38 +112,23 @@ function App() {
 
         <div id="right">
           <div id="form-div">
-            <form>
+            <form ref={formReset} onSubmit={handleSubmit}>
               <label>Cardholder Name</label>
-              <input
-                type="text"
-                placeholder="cardholder name"
-                onChange={(e) => setName(e.target.value)}
-              />
+              <input type="text" name="name" placeholder="cardholder name" />
+              {<p className="error">{nameError}</p>}
               <label>Card Number</label>
-              <input
-                type="text"
-                placeholder="card number"
-                onChange={(e) => setCardNumber(e.target.value)}
-              />
+              <input type="text" name="cardNumber" placeholder="card number" />
+              {<p className="error">{cardNumberError}</p>}
               <label>Month</label>
-              <input
-                type="text"
-                placeholder="MM"
-                onChange={(e) => setMonth(e.target.value)}
-              />
+              <input type="text" name="month" placeholder="MM" />
+              <p className="error">{monthError}</p>
               <label>Year</label>
-              <input
-                type="text"
-                placeholder="YY"
-                onChange={(e) => setYear(e.target.value)}
-              />
+              <input type="text" name="year" placeholder="YY" />
+              <p className="error">{yearError}</p>
               <label>CVC</label>
-              <input
-                type="number"
-                placeholder="CVC"
-                onChange={(e) => setCvc(e.target.value)}
-              />
-              <button type="button">Proceed</button>
+              <input type="number" name="cvc" placeholder="CVC" />
+              <p className="error">{cvcError}</p>
+              <button type="submit">Proceed</button>
             </form>
           </div>
         </div>
